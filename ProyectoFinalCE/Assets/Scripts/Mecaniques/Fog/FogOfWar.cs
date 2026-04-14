09a421207ca91731495e2ee6b8cce60e80913446
Enne <nsamosag@gmail.com>
2026-04-14 15:30:01 +0200

    empezando fog of war

---
+++ ProyectoFinalCE/Assets/Scripts/Mecaniques/Fog/FogOfWar.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FogOfWar : MonoBehaviour
+{
+    #region VARIABLES
+    enum FogState
+    {
+        NON_EXPLORED,
+        EXPLORED,
+        VISIBLE
+    }
+    #endregion
+}
