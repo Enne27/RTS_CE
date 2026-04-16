a27f64e58f746f26701b84133c05e4d87e906901
ANUKGUERRA <anukguerra@gmail.com>
2026-04-16 23:25:09 +0200

    ZonaRecursos

---
+++ ProyectoFinalCE/Assets/Scripts/Mecaniques/ZonasRecursos/ZonaRecursos.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonaRecursos : MonoBehaviour
+{
+    //private List<Ant> farmingAnts = new List<Ant>();
+    //private void Update()
+    //{
+    //    foreach (var ant in farmingAnts)
+    //    {
+            
+    //    }
+    //}
+}
