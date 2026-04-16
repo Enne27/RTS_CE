bf53cddf35e255a0758b3a3a08608ec2d9b9d3e6
Enne43 <naiara4rtb@gmail.com>
2026-04-16 18:35:27 +0200

    Empezando HUD del juego

--- ProyectoFinalCE/Assets/Scripts/UI/Views/GameHUDView.cs
+++ ProyectoFinalCE/Assets/Scripts/UI/Views/GameHUDView.cs
@@ -1,13 +1,36 @@
+using DG.Tweening.Core.Easing;
+using TMPro;
 using UnityEngine;
 
 public class GameHUDView : View
 {
     #region VARIABLES
-
+    [Header("Texts Resources")]
+    [SerializeField] TextMeshProUGUI foodText;
+    [SerializeField] TextMeshProUGUI roeText;
+    [SerializeField] TextMeshProUGUI mcText; // Materiales de construcción
+    [SerializeField] TextMeshProUGUI antWorkersText;
     #endregion
 
     public override void Initialize()
     {
-        
+        /*if(foodText == null) 
+            foodText.text = GameManager.instance.startingFood.toString();
+        if (roeText == null) 
+            // roeText.text = GameManager.instance.startingRoe.toString();
+        if(mcText == null)
+            mcText.text = GameManager.instance.startingMC.toString();
+        if (antWorkersText == null) 
+            antWorkersText.text = GameManager.instance.startingAntWorkers.toString();*/
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="num">Número tanto positivo como negativo.</param>
+    public void UpdateFoodText(int num)
+    {
+        //int newValue = GameManager.instance.startingFood += num;
+        //foodText.text = newValue.toString();
     }
 }
