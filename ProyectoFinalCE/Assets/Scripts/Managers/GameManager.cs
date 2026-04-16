c636cc5421709b0642f1841ccb362a1d4b0baf24
Enne43 <naiara4rtb@gmail.com>
2026-04-16 20:19:36 +0200

    Modified starting values GameManager & resetValues Inventory

--- ProyectoFinalCE/Assets/Scripts/Managers/GameManager.cs
+++ ProyectoFinalCE/Assets/Scripts/Managers/GameManager.cs
@@ -20,19 +20,30 @@ public class GameManager : MonoBehaviour
     #endregion
 
     #region VARIABLES
-    [Header("Starting Values")]
+    [Header("Starting Values (Player Resources)")]
     [Tooltip("Cantidad inicial de comida que posee el jugador.")]
     public int startingFood = 96;
 
     [Tooltip("Cantidad inicial de huevas que posee el jugador al inicio.")]
-    public int startingRoe = 0;
+    public int startingEggs = 0;
 
     [Tooltip("Cantidad inicial de materiales de construcción.")]
     public int startingMC = 0;
 
+    [Tooltip("Cantidad inicial de hormigas obreras.")]
+    public int startingWorkerAnts = 4;
+
+    [Tooltip("Cantidad inicial proporcionada de hormigas exploradoras.")]
+    public int startingExplorerAnts = 4;
+
+
+    [Header("Starting structures (Map constructions)")]
+    public int antHillsQuantity = 2;
+    public int resourcesZoneQuantity = 2;
+
 
     [Header("Player resources")]
-    Player player; // jugador
+    Player player; // Jugador
     Player playerIA; // IA
 
 
@@ -45,5 +56,7 @@ public class GameManager : MonoBehaviour
     {
         player.inventory.ResetAllVariables();
         playerIA.inventory.ResetAllVariables();
+
+        // FALTA AÑADIR LAS OTRAS HORMIGAS INICIALES
     }
 }
