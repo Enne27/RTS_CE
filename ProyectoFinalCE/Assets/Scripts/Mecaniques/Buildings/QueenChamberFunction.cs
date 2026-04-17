a1bfc48403aeb95a989df83caf519946af1357bf
Enne43 <naiara4rtb@gmail.com>
2026-04-17 19:54:09 +0200

    CÁMARA reina en proceso

--- ProyectoFinalCE/Assets/Scripts/Mecaniques/Buildings/QueenChamberFunction.cs
+++ ProyectoFinalCE/Assets/Scripts/Mecaniques/Buildings/QueenChamberFunction.cs
@@ -7,11 +7,13 @@ public class QueenChamberFunction : MonoBehaviour
     [Tooltip("Tiempo que tarda en producir huevas.")]
     [SerializeField] float timeToProduceEggs = 60f;
 
-    [Tooltip("Cantidad de huevas que produce por minuto.")]
-    [SerializeField] float quantityProduction;
-
     [Tooltip("Nivel del edificio.")]
     private int lvl = 1;
+
+    [Tooltip("Cantidad de huevas que produce por minuto.")]
+    [SerializeField] float quantityProduction;
+    // DICCIONARIO O LO QUE SEA DE LOS VALORES DE CANTIDAD DE PRODUCCIÓN, COSTE DE HUEVAS, COSTE DE MC POR NIVEL.
+    // EN IA HAY MÁS COSAS PARA ESTOS
     #endregion
 
     private void OnEnable()
@@ -24,9 +26,17 @@ public class QueenChamberFunction : MonoBehaviour
         TimeManager.Instance.Unregister(timeToProduceEggs, ProduceEggs);
     }
 
+    /// <summary>
+    /// FALTARÍA FEEDBACK, PERO SE GENERAN CADA 60 SEGUNDOS
+    /// </summary>
     public void ProduceEggs()
     {
-       
-       // if () { }
+       int currentEggs = GameManager.instance.player.inventory.eggs;
+       int currentEggCapacity = GameManager.instance.player.inventory.eggCapacity;
+
+       if (currentEggs < currentEggCapacity) 
+       {
+            GameManager.instance.player.inventory.eggs += currentEggCapacity - currentEggs;
+       }
     }
 }
