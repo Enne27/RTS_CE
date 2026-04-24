56bf303ed4316b647e1206e5e76bfe8078ea3806
JS1331 <jansarda2003@gmail.com>
2026-04-24 19:55:30 +0200

    AntActions Updated

--- ProyectoFinalCE/Assets/Scripts/StateMachineFolder/Actions/AntAction.cs
+++ ProyectoFinalCE/Assets/Scripts/StateMachineFolder/Actions/AntAction.cs
@@ -1,12 +1,18 @@
 
 using UnityEngine;
 using StateMachine.Runtime;
+using Unity.Cinemachine;
+using System;
 
 [CreateAssetMenu(fileName = "AntScript", menuName = "Tools/IA/Actions/AntScript")]
 public class AntScript : ScriptableAction
 {
     public override void Execute(StateMachineManager manager)
-    {
-        // TODO: Implement action logic
+    { 
+      void Attack()
+      {
+        Ant ant;
+        //ant.Attack(ant.GetTarget);      
+      }
     }
 }
